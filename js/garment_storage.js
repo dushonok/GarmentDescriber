eb6184fd3d30c4b31c6e88b0ff1fa10ffd406c5f
Samuel Gélineau <gelisam+github@gmail.com>
2014-02-16 16:03:08 -0500

    generate and download CSV separately

--- js/garment_storage.js
+++ js/garment_storage.js
@@ -108,10 +108,13 @@ function getField(sessionId, garmentId, key) {
 }
 
 
-// prompt the user to download a file named "Nadya003.csv"
-// with one line per garment, in the order they were created,
+// one line per garment, in the order they were created,
 // each of which list all the field values (separated by commas)
 // in the order they were added.
 function exportCSV(sessionId) {
   return "Betty Liu,dress,blue,100.0\nNadya Dushonok,bracelet,rainbow,15.0\n";
+}
+
+// prompt the user to download a file named "Nadya003.csv"
+function downloadCSV(sessionId) {
 }
