68377304215afdeeb013813427430086fa0fb3cf
Samuel Gélineau <gelisam+github@gmail.com>
2014-02-16 13:58:00 -0500

    stub everything

--- js/garment_storage.js
+++ js/garment_storage.js
@@ -1,2 +1,23 @@
 function newSession(username) {
 }
+
+function newGarment(sessionId) {
+}
+
+
+function saveField(sessionId, garmentId, key, value) {
+}
+
+
+function listSessions() {
+}
+
+function listGarments(sessionId) {
+}
+
+function getField(sessionId, garmentId, key) {
+}
+
+
+function exportCSV(sessionId) {
+}
