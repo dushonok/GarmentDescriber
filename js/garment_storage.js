49d4a8b2046ac54a92c427195f4c5497e3d22171
Samuel Gélineau <gelisam+github@gmail.com>
2014-02-16 14:08:14 -0500

    move comments to the code.

--- js/garment_storage.js
+++ js/garment_storage.js
@@ -5,6 +5,7 @@ function newGarment(sessionId) {
 }
 
 
+// edit the field if it exists, or add it to the end of the row otherwise
 function saveField(sessionId, garmentId, key, value) {
 }
 
@@ -19,5 +20,9 @@ function getField(sessionId, garmentId, key) {
 }
 
 
+// prompt the user to download a file named "Nadya003.csv"
+// with one line per garment, in the order they were created,
+// each of which list all the field values (separated by commas)
+// in the order they were added.
 function exportCSV(sessionId) {
 }
