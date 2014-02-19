14b3a22074f47c64804d22f0a21125d3c7495cce
Samuel Gélineau <gelisam+github@gmail.com>
2014-02-18 22:35:41 -0500

    a script to batch-create new vendors
    warning, I haven't tested the throttling code yet, maybe it will
    terminate with "FAILED" instead of waiting for 60 seconds. There just
    wasn't enough designers in the list to reach the limit :)

---
+++ create_designers.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -e
+
+function create {
+  OUT="$(curl --user 3c9830eea3fa5afd760748ac941feb1a0b97c890f94734c6b16b7d036e816e4d:apikey --data '{"name": "'"$1"'"}' 'https://api.merchantos.com/API/Account/71231/Vendor.json')"
+  if [ "$(echo "$OUT" | grep '{"count":"1"}')" ]; then
+    echo "created $1."
+  elif [ "$(echo "$OUT" | grep "HTTP/1.1 503")" ]; then
+    echo "Waiting 60 seconds due to API throttling."
+    sleep 60
+    delete "$1"
+  else
+    echo FAILED
+    echo "$OUT"
+    exit 1
+  fi
+}
+
+
+create "Les Enfants Sauvages"
+create "Betina Lou"
+create "Bohten"
+create "DIY Couture"
+create "IOU"
+create "Meemoza"
+create "Komodo"
+create "Goodone"
+create "Rachel F"
+create "Uranium"
+create "Norwegian Wood"
+create "PACT"
+create "Bolilaine"
+create "Green Bijoux"
+create "Bellemine"
+create "3D Rings"
+create "Papiroga"
+create "Crowned Bird"
