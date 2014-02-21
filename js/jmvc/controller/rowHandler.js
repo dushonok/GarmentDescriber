45ca5bff9cd3785ecaf70a86dfa1848c61731c1c
dushonok <nadezhda18@gmail.com>
2014-02-20 22:47:06 -0500

    Fixed value passing on Save & REstart button click

--- js/jmvc/controller/rowHandler.js
+++ js/jmvc/controller/rowHandler.js
@@ -2,13 +2,23 @@ function RowHandler() {}
 
 var clickedValue;
 
-RowHandler.itemWasClicked = function(inputName, takeInputValue) {
-		var realValue = takeInputValue ? document.getElementById(inputName).value : clickedValue;
-		RowHandler.pageHandler.setCurrentID(realValue);
-		RowHandler.pageHandler.goToNextPage();
-	}
 
 RowHandler.setValue = function(inputName, value, valueToShow) {
 	clickedValue = value;
 	document.getElementById(inputName).value = valueToShow;
+}
+
+RowHandler.saveValue = function(inputName, takeInputValue) {
+	var realValue = takeInputValue ? document.getElementById(inputName).value : clickedValue;
+	RowHandler.pageHandler.setCurrentID(realValue);
+}
+
+RowHandler.saveValueAndGoToNext = function(inputName, takeInputValue) {
+	RowHandler.saveValue(inputName, takeInputValue);
+	RowHandler.pageHandler.goToNextPage();
+}
+
+RowHandler.saveAndRestart = function(inputName, takeInputValue) {
+	RowHandler.saveValue(inputName, takeInputValue);
+	RowHandler.pageHandler.saveAndRestart();
 }
