7cee995748b34e3c0e79f3c418a7f337a3ab1709
Samuel Gélineau <gelisam@gmail.com>
2014-05-17 16:50:16 -0600

    add logout buttons everywhere

---
+++ logout.php
@@ -0,0 +1,6 @@
+<?
+$token = "";
+file_put_contents("private/token", $token);
+
+?>
+<p>OK, you've been logged out. Click <a href="https://cloud.merchantos.com/oauth/authorize.php?response_type=code&client_id=garment_describer&scope=employee:inventory">here</a> if you want to log in again.</p>
