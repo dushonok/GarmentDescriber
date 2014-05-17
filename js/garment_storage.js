0c86372c1bfbd5fc800fe57e1e9f3b4c02656191
Samuel Gélineau <gelisam@gmail.com>
2014-05-17 16:42:44 -0600

    hide unsupported field types

--- js/garment_storage.js
+++ js/garment_storage.js
@@ -194,7 +194,7 @@ function downloadCSV(sessionId) {
 
 
 function listFieldKeys() {
-  return ["Category", "TaxClass", "ItemMatrix", "Manufacturer", "Vendor", "Consignors"];
+  return ["Category", /*"TaxClass", "ItemMatrix",*/ "Manufacturer", /*"Vendor",*/ "Consignors"];
 }
 
 function listFieldValues(fieldKey, callback) {
