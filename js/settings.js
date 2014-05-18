f44dc6af6400e799a65468525e5b8355378f3b35
dushonok <nadezhda18@gmail.com>
2014-05-17 23:00:46 -0400

    Put logout url into settings

--- js/settings.js
+++ js/settings.js
@@ -1,4 +1,8 @@
 function Settings() {
 }
 
+Settings.getLogoutUrl = function() {
+	return "logout.php";
+}
+
 Settings.loginUrl = "https://cloud.merchantos.com/oauth/authorize.php?response_type=code&client_id=garment_describer&scope=employee:all"
