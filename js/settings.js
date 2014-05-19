140e78b676c4c81880483bfa25fbe207d477cc07
dushonok <nadezhda18@gmail.com>
2014-05-19 14:34:19 -0400

    Fixed logout url

--- js/settings.js
+++ js/settings.js
@@ -2,7 +2,7 @@ function Settings() {
 }
 
 Settings.getLogoutUrl = function() {
-	return "logout.php";
+	return "logout.php?code=" + getOAuthCode(getDefaultSessionId());
 }
 
 Settings.loginUrl = "https://cloud.merchantos.com/oauth/authorize.php?response_type=code&client_id=garment_describer&scope=employee:all"
